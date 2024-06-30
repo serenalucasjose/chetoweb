886a453a58d696a3d16e8c3132fc1de59f766006
unknown <serenalucasjose@gmail.com>
2024-06-30 19:51:11 -0300

    sacado el mp link

--- src/js/main.js
+++ src/js/main.js
@@ -10,29 +10,30 @@ window.closeDialog = function closeDialog() {
     // Update the URL without reloading the page
     history.pushState({}, '', baseUrl);
 }
-// MP
+
 document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const pagoExitoso = urlParams.get('pagoexitoso');
-    const pagoEnProceso = urlParams.get('pagoenproceso');
-    // Set msg
-    if(pagoExitoso || pagoEnProceso) {
-        let msg = (pagoExitoso) ? 
-            `
-            <p>Tu pago fue exitoso</p>
-            <p>En breve recibirás un mail con la confirmación a la dirección de correo asociada a tu cuenta MercadoPago.</p>
-            <p>Ante cualquier duda, también puedes enviar el comprobante de pago a nuestra cuenta de Instagram <a href="https://www.instagram.com/chetomusica?igsh=NTU1bXM5MDQ1MjIy" rel="noopener" target="_blank">@chetomusica</a></p>
-            <p>Nos vemos pronto!</p>
-            ` :
-            `
-            <p>Tu pago esta en proceso</p>
-            <p>En breve recibirás un mail con la confirmación a la dirección de correo asociada a tu cuenta MercadoPago.</p>
-            <p>Ante cualquier duda, también puedes enviar el comprobante de pago a nuestra cuenta de Instagram <a href="https://www.instagram.com/chetomusica?igsh=NTU1bXM5MDQ1MjIy" rel="noopener" target="_blank">@chetomusica</a></p>
-            <p>Nos vemos pronto!</p>
-            `
-        // Set the message to the dialog
-        document.getElementById('dialog-msg').innerHTML = msg;
-        // Display dialog
-        document.getElementById('cheto-dialog').showModal();
-    }
+    /* MP */
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const pagoExitoso = urlParams.get('pagoexitoso');
+    // const pagoEnProceso = urlParams.get('pagoenproceso');
+    // // Set msg
+    // if(pagoExitoso || pagoEnProceso) {
+    //     let msg = (pagoExitoso) ? 
+    //         `
+    //         <p>Tu pago fue exitoso</p>
+    //         <p>En breve recibirás un mail con la confirmación a la dirección de correo asociada a tu cuenta MercadoPago.</p>
+    //         <p>Ante cualquier duda, también puedes enviar el comprobante de pago a nuestra cuenta de Instagram <a href="https://www.instagram.com/chetomusica?igsh=NTU1bXM5MDQ1MjIy" rel="noopener" target="_blank">@chetomusica</a></p>
+    //         <p>Nos vemos pronto!</p>
+    //         ` :
+    //         `
+    //         <p>Tu pago esta en proceso</p>
+    //         <p>En breve recibirás un mail con la confirmación a la dirección de correo asociada a tu cuenta MercadoPago.</p>
+    //         <p>Ante cualquier duda, también puedes enviar el comprobante de pago a nuestra cuenta de Instagram <a href="https://www.instagram.com/chetomusica?igsh=NTU1bXM5MDQ1MjIy" rel="noopener" target="_blank">@chetomusica</a></p>
+    //         <p>Nos vemos pronto!</p>
+    //         `
+    //     // Set the message to the dialog
+    //     document.getElementById('dialog-msg').innerHTML = msg;
+    //     // Display dialog
+    //     document.getElementById('cheto-dialog').showModal();
+    // }
 });
